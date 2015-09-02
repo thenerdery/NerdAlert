718a45691e5b7c85e54278259b86eb185dbd449f
Richard Banasiak <richard@banasiak.com>
2015-09-02 16:36:29 -0500

    Layout refactoring.

---
+++ app/src/main/java/com/banasiak/android/nerdalert/ui/activity/MainActivity.java
@@ -0,0 +1,72 @@
+package com.banasiak.android.nerdalert.ui.activity;
+
+import com.banasiak.android.nerdalert.R;
+import com.banasiak.android.nerdalert.ui.adapters.SectionsPagerAdapter;
+import com.banasiak.android.nerdalert.ui.slidingtabs.SlidingTabLayout;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // set the navigation bar color on Lollipop+ devices
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.color_primary_dark));
+        }
+
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+
+        SlidingTabLayout tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.color_accent));
+        tabs.setTextColor(ContextCompat.getColor(this, R.color.color_text_light));
+        tabs.setDividerColors(ContextCompat.getColor(this, R.color.color_divider));
+        tabs.setViewPager(viewPager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+}
